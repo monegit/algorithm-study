be854cf90bb9d61d7a5d841e81bddd535ce707d3
Jongwon Park <pjw970220@gmail.com>
2020-05-20 17:33:01 +0900

    📖Baekjoon NO.1003 Problem Solved

--- Baekjoon/10872/p10872.py
+++ Baekjoon/10872/p10872.py
@@ -10,27 +10,3 @@ def factorial(num):
         return result * num
 
 print(factorial(input_factorial))
-
-# input_factorial = int(input())
-
-# def factorial(num):
-#     result = 1
-#     if num == 0:
-#         return 1
-#     else:
-#         for i in range(1, num):
-#             result *= i
-#         return result * num
-
-# print(factorial(input_factorial))
-
-# input_factorial = int(input())
-
-# def factorial(num):
-#     result = 1
-#     for i in range(1, num):
-#         result *= i
-#     return result * num
-
-# for i in range(12):
-#     print(factorial(i))
