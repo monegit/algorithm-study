46fd9eb46e92e6204d65875a1e5a5f48e71e761f
Jongwon Park <pjw970220@gmail.com>
2020-06-06 00:57:46 +0900

    Space/Dotnet Update

--- Space/Dotnet/Program.cs
+++ Space/Dotnet/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Dotnet
 {
@@ -6,9 +7,23 @@ namespace Dotnet
     {
         static void Main(string[] args)
         {
-            string str = string.Empty;
-            str = Console.ReadLine();
-            Console.WriteLine(str);
+            StringBuilder sb = new StringBuilder();
+
+            string str = Console.ReadLine();
+            int n = int.Parse(str.Split(" ")[0]);
+            int m = int.Parse(str.Split(" ")[1]);
+            int sum = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int ii = 0; ii < m; ii++)
+                {
+                    sb.Append(sum+" ");
+                    sum++;
+                }
+                Console.WriteLine(sb.ToString().Trim());
+                sb.Clear();
+            }
         }
     }
 }
