5ac7eceb22022a8f529c8462d53eb95e5aecdd50
Jongwon Park <pjw970220@gmail.com>
2020-06-06 00:55:04 +0900

    Bubble Sort Write Code as GoLang

--- Personal/BubbleSort/go.go
+++ Personal/BubbleSort/go.go
@@ -3,6 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	a := []int{1, 2, 3, 4, 5}
-	fmt.Printf("%v\n", a)
+	arr := []int{2, 3, 4, 1, 5}
+
+	fmt.Printf("%v\n", StartToEndBubbleSort(arr))
+}
+
+func StartToEndBubbleSort(array []int) []int {
+	arr := array
+	temp := 0
+
+	for ii := 0; ii < 4; ii++ {
+		for i := 0; i < len(arr)-1; i++ {
+			if arr[i] > arr[i+1] {
+				temp = arr[i]
+				arr[i] = arr[i+1]
+				arr[i+1] = temp
+			}
+		}
+	}
+
+	return arr
 }
