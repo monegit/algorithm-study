f09f8a1d1dd610495dbb2bd8f3e82ced100b4013
Jongwon Park <pjw970220@gmail.com>
2020-06-13 07:06:16 +0900

    add example input.go

--- Space/Example/Go/input.go
+++ Space/Example/Go/input.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 )
 
 func main() {
+	// #1
 	scanner := bufio.NewScanner(os.Stdin)
 	if scanner.Scan() {
 		println(scanner.Text())
 	}
 
+	// #2
+	var i int
+	fmt.Scanf("%d", &i)
+
+	fmt.Printf("%d", i)
 }
