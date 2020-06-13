6a335581da8d1323ad68aaf30c0b5d20c731bd8a
Jongwon Park <pjw970220@gmail.com>
2020-06-13 12:10:24 +0900

    add sample in Example/input.python

--- Space/Example/Python/input.py
+++ Space/Example/Python/input.py
@@ -1,4 +1,18 @@
+# Nomal Int Type Input
 _input = int(input())
 
 for i in range(_input):
     print(i)
+
+# List Type Input
+_input = list(input().split())
+
+for i in range(len(_input)):
+    print(i)
+
+# Practice Input
+# _input array each value plus 1
+_input = list(map(int, input().split()))
+
+for i in range(len(_input)):
+    print(i+1)
