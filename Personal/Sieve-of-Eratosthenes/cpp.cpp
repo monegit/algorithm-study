be41648d2ac81893f46b0130228d5939b8314056
Jongwon Park <pjw970220@gmail.com>
2020-07-20 16:19:48 +0900

    Sieve of Eratosthenes Algorithm Written with C++

---
+++ Personal/Sieve-of-Eratosthenes/cpp.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+void Eratosthenes(int n) {
+    if(n<=1) return;
+
+    bool* PrimeArray = new bool[n + 1];
+
+    for (int i = 2; i <= n; i++)
+        // PrimeArray의 모든 값들을 소수로 임시 정의
+        PrimeArray[i] = true;
+
+    for (int i = 2; i * i <= n; i++)
+        if (PrimeArray[i])
+            for (int j = i * i; j <= n; j+=i)
+                PrimeArray[j] = false;
+
+    for (int i = 0; i <= n; i++) {
+        if (PrimeArray[i])
+            cout<< i << ", ";
+    }
+}
+
+int main() {
+    Eratosthenes(100);
+
+    return 0;
+}
