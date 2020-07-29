77693bcc0b9d5642e88e4651d822834b5a9d1d1d
Jongwon Park <pjw970220@gmail.com>
2020-07-29 21:32:55 +0900

    BOJ NO.1065 Problem Solved with Python

---
+++ Training-Site/Baekjoon/1/1000/1065/Python/1065.py
@@ -0,0 +1,11 @@
+_input = int(input())
+count = 0
+
+for i in range(1, _input + 1):
+    if i < 100:
+        count+=1
+    else:
+        arr = list(map(int, str(i)))
+        if arr[0] - arr[1] == arr[1] - arr[2]:
+            count+=1
+            break
