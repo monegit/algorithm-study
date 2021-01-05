9ac2ec8e8ebf82e85242ce9258499b767a567052
Jongwon Park <pjw970220@gmail.com>
2021-01-05 09:59:35 +0900

    속도로 인한 오답

---
+++ Training-Site/Baekjoon/1/1000/2869/Fail/source.py
@@ -0,0 +1,14 @@
+# 속도로 인한 오답
+
+input = list(map(int, input().split()))
+
+day, run = 0, 0
+
+while True:
+    day += 1
+    run +=input[0]
+    if run >= input[2]:
+        break
+    run-=input[1]
+
+print(day)
