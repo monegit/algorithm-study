9cf63c91a10aba1a56fbc86e2d79a8b950e0577f
Jongwon Park <pjw970220@gmail.com>
2022-09-10 04:12:57 +0900

    주석 추가

--- Training-Site/To-Do/Baekjoon/2775.py
+++ Training-Site/To-Do/Baekjoon/2775.py
@@ -1,16 +1,20 @@
-# 차트 만들기
+# 배열 만들기
 apart = []
 
+# 15개의 층을 만든다.
 for i in range(15):
     apart.append([])
 
+# 0층의 각 호수별 인원을 구한다.
 for i in range(14):
     apart[0].append(i+1)
 
+# 배열 0, 1의 인원을 구한다.
 for i in range(1, 15):
     apart[i].append(1)
     apart[i].append(i+2)
 
+# 1층부터 14층 까지 인원을 넣는다.
 for y in range(1, 15):
     for x in range(2, 14):
         apart[y].append(
