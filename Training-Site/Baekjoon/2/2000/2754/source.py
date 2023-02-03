4179d830eb71ed3cc25e8fccfab4ad434ed7f051
Jongwon Park <pjw970220@gmail.com>
2023-02-03 23:47:11 +0900

    백준 2754번: 학점계산

---
+++ Training-Site/Baekjoon/2/2000/2754/source.py
@@ -0,0 +1,28 @@
+input = input()
+
+if input == "A+":
+    print(4.3)
+elif input == "A0":
+    print(4.0)
+elif input == "A-":
+    print(3.7)
+elif input == "B+":
+    print(3.3)
+elif input == "B0":
+    print(3.0)
+elif input == "B-":
+    print(2.7)
+elif input == "C+":
+    print(2.3)
+elif input == "C0":
+    print(2.0)
+elif input == "C-":
+    print(1.7)
+elif input == "D+":
+    print(1.3)
+elif input == "D0":
+    print(1.0)
+elif input == "D-":
+    print(0.7)
+elif input == "F":
+    print(0.0)
