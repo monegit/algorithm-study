bf774ab8dddf955f23d640502880cddc3238c898
Jongwon Park <pjw970220@gmail.com>
2025-05-21 21:45:28 +0900

    프로그래머스 'a와 b 출력하기'

---
+++ Training-Site/Programmers/181951.js
@@ -0,0 +1,16 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181951
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input = line.split(" ");
+}).on("close", function () {
+  console.log(`a = ${input[0]}`);
+  console.log(`b = ${input[1]}`);
+});
