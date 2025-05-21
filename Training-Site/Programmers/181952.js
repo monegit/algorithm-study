fa9102ed26a03497d4aacdd81b136b469c65fbaa
Jongwon Park <pjw970220@gmail.com>
2025-05-21 21:42:34 +0900

    프로그래머스 '문자열 출력하기'

---
+++ Training-Site/Programmers/181952.js
@@ -0,0 +1,15 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181952
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input = line;
+}).on("close", function () {
+  console.log(input);
+});
